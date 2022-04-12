f23a05df315714d7cec07f295b05f60ca1dc8ed9
Quentin Pongratz <qpongratz@gmail.com>
2022-04-12 16:44:44 -0500

    Setup attendances associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :host, class_name: 'User'
-
+  has_many :attendances, foreign_key: 'attended_event_id'
+  has_many :event_attendees, through: :attendances
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :hosted_events, foreign_key: 'host_id', class_name: 'Event', inverse_of: 'host'
+  has_many :attendances, foreign_key: 'event_attendee_id'
+  has_many :attended_events, through: :attendances
 end
