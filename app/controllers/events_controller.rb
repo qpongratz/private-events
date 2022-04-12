46249db737749afa11a6d35159e2208e11209cbd
Quentin Pongratz <qpongratz@gmail.com>
2022-04-12 15:41:18 -0500

    Add event controller actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,26 @@
 class EventsController < ApplicationController
-
   def index
     @events = Event.all
   end
 
+  def new
+    @event = Event.new(event_params)
+  end
+
+  def create
+    @event = current_user.hosted_events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+      flash[:notice] = 'Event successfully created.'
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :location)
+  end
 end
