84744f74b436b5da7f8bb9048f6d47a3a5a8cc04
Quentin Pongratz <qpongratz@gmail.com>
2022-04-13 10:59:05 -0500

    Refactor past and upcoming to class methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :host, class_name: 'User'
   has_many :attendances, foreign_key: 'attended_event_id'
   has_many :event_attendees, through: :attendances
+
+  def self.past
+    where('date < ?', Date.today)
+  end
+
+  def self.upcoming
+    where('date >= ?', Date.today)
+  end
 end
