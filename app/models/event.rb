51c67b1962947f652504bda49f107c8648159875
Quentin Pongratz <qpongratz@gmail.com>
2022-04-13 15:51:33 -0500

    Add data validation to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,9 @@ class Event < ApplicationRecord
   belongs_to :host, class_name: 'User'
   has_many :attendances, foreign_key: 'attended_event_id'
   has_many :event_attendees, through: :attendances
+
+  validates :title, :description, :date, :location, presence: true
+
   scope :past, -> { where('date < ?', Date.today) }
   scope :upcoming, -> { where('date >= ?', Date.today) }
 end
